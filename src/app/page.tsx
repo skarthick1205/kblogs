a75999e26eec2653b910672c7029b9e7d15834a7
skarthick1205 <karthick1205tirunelveli@gmail.com>
2024-08-31 12:12:38 +0530

    add section connected to firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+
 import Image from "next/image";
 
 
@@ -6,9 +8,12 @@ import Image from "next/image";
 export default function Home() {
  
   return (
+    
     <div>
       <h1>home12</h1>
-     
+    
     </div>
+     
+
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,18 @@ import Modal from 'react-modal';
 import { IoMdAddCircleOutline } from 'react-icons/io';
 import { useEffect, useRef, useState } from 'react';
 
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+
+
+
 
 
 
@@ -20,6 +32,44 @@ export default function Header() {
   const [image, setImage] = useState(null);
   console.log(image);
   const [loading, setLoading] = useState(false);
+
+   //Create data post and add it to the collection
+   const uploadPost = async () => {
+    setLoading(true);
+    const docRef = await addDoc(collection(db, "posts"), {
+      profileImg: session?.user?.image,
+      username: session?.user?.name,
+      caption: captionRef.current.value,
+      timestamp: serverTimestamp(),
+    });
+    //Path for the image
+    const imagePath = ref(storage, `posts/${docRef.id}/image`);
+
+    //Upload image to that adress
+    //Then with the snapshot declare the download URL
+    await uploadString(imagePath, image, "data_url").then(async (snapshot) => {
+      const downloadURL = await getDownloadURL(imagePath);
+      await updateDoc(doc(db, "posts", docRef.id), {
+        image: downloadURL,
+      });
+    });
+    setImage("");
+    setLoading(false);
+    captionRef.current.value = null;
+  };
+
+  //Add the image to state
+  const addImageToState = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setImage(readerEvent.target.result);
+    };
+  };
+
+
 
   return (
     <div className="shadow-lg border-b sticky top-0 bg-white z-30 p-3">
@@ -76,55 +126,49 @@ export default function Header() {
       </div>
       {isOpen && (
         <Modal
-          isOpen={isOpen}
-          className='max-w-3xl w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
-          onRequestClose={() => setIsOpen(false)}
-          ariaHideApp={false}
-        >
-          
-          <div className=" flex items-center  h-full shrink-0 w-full">
-            <img
-              src={session ? session?.user?.image : nouser.src}
-              className="rounded-full "
-            />
-              <input
-              type="text"
-              placeholder="What's on your mind Joe Doe?"
-              className="outline-0 bg-[#f2f3f7] p-1 ml-5 rounded-full pl-3 w-full h-12 truncate"
-              ref={captionRef}
-            />
-            <div
-            className="flex items-center bg-blue-500 px-3 rounded-full h-10 ml-4"
-          
+        isOpen={isOpen}
+        className='w-full max-w-3xl p-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border-2 rounded-md shadow-md'
+        onRequestClose={() => setIsOpen(false)}
+        ariaHideApp={false}
+      >
+        <div className="flex flex-col sm:flex-row items-center sm:h-full w-full">
+          <img
+            src={session ? session?.user?.image : nouser.src}
+            className="rounded-full h-12 w-12"
+          />
+          <input
+            type="text"
+            placeholder="What's on your mind Joe Doe?"
+            className="outline-0 bg-[#f2f3f7] p-2 sm:ml-5 rounded-full w-full sm:h-12 truncate mt-4 sm:mt-0"
+            ref={captionRef}
+          />
+          <div
+            className="flex items-center bg-blue-500 px-3 rounded-full h-10 sm:ml-4 mt-4 sm:mt-0"
+            onClick={uploadPost}
           >
             <button className="font-bold text-white">
-            {loading ? "Loading" : "Post"}
+              {loading ? "Loading" : "Post"}
             </button>
           </div>
-          </div>
-          <div className="">
-          {image ? (
-            <div className="" onClick={() => setImage("")}>
-              <img src={image} className="p-4" alt="img" />
+        </div>
+        <div className="mt-4">
+          {image && (
+            <div onClick={() => setImage("")}>
+              <img src={image} className="p-4 max-h-60 object-cover mx-auto" alt="img" />
             </div>
-          ) : (
-            ""
           )}
         </div>
-
-        <div
-          className="
-        border-b mb-4 mt-2"
-        ></div>
-
-<div className="flex justify-between px-3 sm:mx-9 pb-3">
+    
+        <div className="border-b mb-4 mt-2"></div>
+    
+        <div className="flex justify-between px-3 sm:mx-9 pb-3">
           <div className="flex items-center">
             <div className="w-7 h-7">
               <Image src="/camera.png" width={30} height={30}/>
             </div>
-            <p className="pl-2  whitespace-nowrap text-[14px]">Live Video</p>
+            <p className="pl-2 whitespace-nowrap text-[14px]">Live Video</p>
           </div>
-
+    
           <div
             className="flex items-center"
             onClick={() => imageRef.current.click()}
@@ -135,26 +179,20 @@ export default function Header() {
                 type="file"
                 className="hidden"
                 ref={imageRef}
-                
+                onChange={addImageToState}
               />
             </div>
-            <p className="pl-2   text-[14px]">Photo/Video</p>
+            <p className="pl-2 text-[14px]">Photo/Video</p>
           </div>
-
+    
           <div className="flex items-center">
             <div className="w-7 h-7">
               <Image src="/smile.png" width={30} height={30}/>
             </div>
-            <p className="pl-2   text-[14px]">Feeling/Activity</p>
+            <p className="pl-2 text-[14px]">Feeling/Activity</p>
           </div>
         </div>
-          
-
-          
-          
-          
-          
-        </Modal>
+      </Modal>
       )}
     </div>
   );
